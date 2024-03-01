f6aa15d9dac9dcdc5aa5759e50e551d3f7f12d5b
Rakesh Kashyap <161632766+rakeshthefiendcoder@users.noreply.github.com>
2024-03-01 19:51:25 +0530

    Add files via upload

---
+++ Day 1/Java Script/Day1.js
@@ -0,0 +1,7 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
